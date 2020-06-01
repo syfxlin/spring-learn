01f1551ed06d52b9881b47ba80bfedde1d815db8
syfxlin <syfxlin@gmail.com>
2020-06-01 14:51:44 +0800

    :sparkles: feat: Add mock test

--- test/src/main/java/me/ixk/test/TestApplication.java
+++ test/src/main/java/me/ixk/test/TestApplication.java
@@ -2,12 +2,19 @@ package me.ixk.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
     }
 
+    @GetMapping("/get")
+    public String get(String key) {
+        return key;
+    }
 }
--- test/src/test/java/me/ixk/test/TestApplicationTests.java
+++ test/src/test/java/me/ixk/test/TestApplicationTests.java
@@ -3,7 +3,15 @@ package me.ixk.test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
 class TestApplicationTests {
@@ -22,6 +30,30 @@ class TestApplicationTests {
     public void testAdd() {
         System.out.println("Test");
         assertEquals(1 + 1, 2);
+    }
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @BeforeEach
+    public void setupMockMvc() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    public void testMock() {
+        try {
+            MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+            params.add("key", "value");
+            mockMvc
+                .perform(MockMvcRequestBuilders.get("/get").params(params))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("value"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @AfterAll
