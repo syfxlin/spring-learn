6b339ea52b85381d4f776d5b9d90784d7c8d16d8
syfxlin <syfxlin@gmail.com>
2020-06-01 16:57:09 +0800

    :art: refactor: Update

--- json/src/main/java/me/ixk/json/utils/JSON.java
+++ json/src/main/java/me/ixk/json/utils/JSON.java
@@ -2,10 +2,20 @@ package me.ixk.json.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JSON extends ObjectMapper {
+    public ObjectNode parseObject(String json) throws JsonProcessingException {
+        return this.readValue(json, ObjectNode.class);
+    }
+
+    public ArrayNode parseArray(String json) throws JsonProcessingException {
+        return this.readValue(json, ArrayNode.class);
+    }
+
     public <T> T parse(String json, Class<T> valueType) throws JsonProcessingException {
         return this.readValue(json, valueType);
     }
