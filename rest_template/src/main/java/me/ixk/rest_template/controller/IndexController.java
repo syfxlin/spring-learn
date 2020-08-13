5472a9b0252fcc544d4f370e6addd23df52b0b8a
syfxlin <syfxlin@gmail.com>
2020-08-13 15:32:27 +0800

    :sparkles: feat: Update RestTemplate

--- rest_template/src/main/java/me/ixk/rest_template/controller/IndexController.java
+++ rest_template/src/main/java/me/ixk/rest_template/controller/IndexController.java
@@ -2,6 +2,9 @@ package me.ixk.rest_template.controller;
 
 import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,9 +23,29 @@ public class IndexController {
 
   @GetMapping("/rest-get")
   public String get() {
-    URI uri = UriComponentsBuilder
+    final URI uri = UriComponentsBuilder
       .fromUriString("http://localhost:8080/{id}")
       .build("1");
     return restTemplate.getForObject(uri, String.class);
+  }
+
+  public void header() {
+    final URI uri = UriComponentsBuilder
+      .fromUriString("http://localhost:8080/{id}")
+      .build("1");
+    RequestEntity<Void> requestEntity = RequestEntity
+      .get(uri)
+      .header("X-Header", "1")
+      .build();
+    ResponseEntity<String> responseEntity = restTemplate.exchange(
+      requestEntity,
+      String.class
+    );
+    ResponseEntity<String> responseEntity1 = restTemplate.exchange(
+      requestEntity,
+      new ParameterizedTypeReference<String>() {
+        // ...
+      }
+    );
   }
 }
